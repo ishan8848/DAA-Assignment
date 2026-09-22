06e5745cbe4a73caf4e1e0d810d8f92c1bfad5b9
Ishan Yadav <yadavishan401@gmail.com>
2026-09-22 08:51:22 +0530

    Implement search in a 2D matrix

---
+++ Assignment -1 /74. Search a 2D Matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int lo = 0, hi = rows * cols - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int val = matrix[mid / cols][mid % cols];
+            if (val == target) {
+                return true;
+            } else if (val < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return false;
+    }
+}
